c52575d33a3d61382bc771652c1efaa189b591fb
NikEmman <121699424+NikEmman@users.noreply.github.com>
2023-10-21 21:01:43 +0300

    Progressed on script.js.
    Next problem to tackle is invalid player choise

--- script.js
+++ script.js
@@ -1,24 +1,31 @@
 function getComputerChoice(){
     let number = Math.floor(Math.random()*3)+1;
-    if (number===1)   {return "Rock;"} 
+    if (number===1)   {return "Rock";} 
     else if (number===2)   {return "Paper";} 
     else return "Scissors";
 };
-const computerSelection = getComputerChoice().toLowerCase();
-const playerSelection = prompt("Type your choice!","").toLowerCase();
+let playerSelection = prompt("Type your choice!","");
 
-function playRound(playerSelection, computerSelection){  
-            
-    if (playerSelection==="rock"&&computerSelection==="paper" ||playerSelection==="paper"&&computerSelection==="scissors" || playerSelection==="scissors"&&computerSelection==="rock" ){
-        return `You lost! ${playerSelection} loses to ${computerSelection}.`;
-    }
-    else if(playerSelection==="rock"&& computerSelection==="scissors" ||playerSelection==="paper"&&computerSelection==="rock" || playerSelection==="scissors"&&computerSelection==="paper"){
-        return `You won!! ${playerSelection} wins over ${computerSelection}.`;
-    }
-    else{
-        return `It's a tie! You chose ${playerSelection} and opponent chose ${computerSelection} too.`;
-    }
-    
+
+let computerSelection= getComputerChoice();
+
+function playRound(string1, string2){ 
+    if(playerSelection.toLowerCase()!=="rock" || playerSelection.toLowerCase()!=="paper" ||playerSelection.toLowerCase()!=="scissors") {
+        playerSelection= prompt("Invalid answer, please choose between Rock, Paper or Scissors!","");
+    } 
+    string1=playerSelection.toLowerCase();
+    string2=computerSelection.toLowerCase();
+                
+    if (string1==="rock"&&string2==="paper" ||string1==="paper"&&string2==="scissors" || string1==="scissors"&&string2==="rock" ){
+            return `You lost! ${playerSelection} loses to ${computerSelection}.`;
+        }
+    else if(string1==="rock"&& string2==="scissors" ||string1==="paper"&&string2==="rock" || string1==="scissors"&&string2==="paper"){
+            return `You won!! ${playerSelection} wins over ${computerSelection}.`;
+        }
+    else {
+            return `It's a tie! You chose ${playerSelection} and opponent chose ${computerSelection} too.`;
+        } 
+     
 }
 
-console.log (playRound());
+console.log (playRound(playerSelection,computerSelection));
