c72b575aeb0124c4c337534930839b833884e967
NikEmman <121699424+NikEmman@users.noreply.github.com>
2023-10-24 12:22:37 +0300

    True completion of the project
    Turns out the project assignment required of a score system and a final game result, so updated the js with one. Over and out!

--- script.js
+++ script.js
@@ -12,6 +12,9 @@ while(pChoice.toLowerCase()!=="rock" && pChoice.toLowerCase()!=="paper" && pChoi
 } 
 return pChoice;
 }
+let win=0
+let loss = 0;
+
 
 
 function playRound(){
@@ -21,24 +24,40 @@ function playRound(){
     
                 
     if (string1==="rock"&&string2==="paper" ||string1==="paper"&&string2==="scissors" || string1==="scissors"&&string2==="rock" ){
-            return `You lost! ${string1} loses to ${string2}.`;
+            loss++;
+            return `You lost the round ${string1} loses to ${string2}.`;
         }
     else if(string1==="rock"&& string2==="scissors" ||string1==="paper"&&string2==="rock" || string1==="scissors"&&string2==="paper"){
-            return `You won!! ${string1} wins over ${string2}.`;
+            win++;
+            return `You won the round ${string1} wins over ${string2}.`;
         }
-    else {
-            return `It's a tie! You chose ${string1} and opponent chose ${string2} too.`;
+    else {            
+            return `It's a tie you chose ${string1} and opponent chose ${string2} too.`;
         } 
     
 }
 
+
 function game(x){    
     for ( let i=0; i<x; i++){
-        
-         console.log (playRound());              
+
+         console.log (playRound());
+                     
     }   
     
 }
 game(5);
-
+let result;
+if (win>loss)
+{
+     result=`Congratulations! You won the game by ${win} - ${loss}`
+}
+else if (win <loss)
+{
+    result=`Oh noes! You lost the game by ${win} - ${loss}`
+}
+else{
+    result=`LOL you tied the game by ${win} - ${loss}`
+}
+console.log(result);
 
